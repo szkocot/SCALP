6d4e005c80707ac8637e951dae32be2d568e8179
wopoczynski <wojtek.opoczynski@gmail.com>
2019-02-02 20:57:39 +0100

    fix merge xD

---
+++ src/app/Service/JsonDataParser.py
@@ -0,0 +1,15 @@
+from src.app.Model import Acquisition, Clinical, Creator, Dataset, Meta, Metadata, Notes, Tags, Unstructured
+
+class JsonDataParser:
+
+    def __init__(self):
+        return
+
+    def parseFile(self):
+        return
+
+    def readFile(self):
+        return
+
+    def getFileList(self):
+        return
