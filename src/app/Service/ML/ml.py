7c62e76be59d3850ef9d1b23911805e240118c45
Szymon Kocot <szymko1995@gmail.com>
2019-02-05 01:30:27 +0100

    Move graph execution to app

--- src/app/Service/ML/ml.py
+++ src/app/Service/ML/ml.py
@@ -1,5 +1,4 @@
 import keras
-import tensorflow as tf
 import config
 from src.app.Service.ML.preprocessing import Preprocessor
 from src.app.Helper.utils import idToPath
@@ -17,7 +16,6 @@ class Predictor(Preprocessor):
 
     def __init__(self):
         super().__init__()
-        self.graph = tf.get_default_graph()
         self.model = keras.models.load_model(config.PREDICTOR['model_path'])
 
     def __call__(self, img_path, mask_path):
@@ -29,8 +27,7 @@ class Predictor(Preprocessor):
 
         # resize to N,W,H,C format (N==1)
         img_ready.shape = (1, *img_ready.shape)
-        with self.graph:
-            y_pred = self.model.predict(img_ready)
+        y_pred = self.model.predict(img_ready)
 
         y_dict = {'benign': y_pred[0][0],
                   'malignant': y_pred[0][1]}
