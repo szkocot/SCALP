cc1e77dfe73c9f4737d14af6bf06d5f52e777035
Szymon Kocot <szymko1995@gmail.com>
2019-02-05 01:28:43 +0100

    Fix model path

--- config.py
+++ config.py
@@ -43,6 +43,6 @@ PREDICTOR = {
     'img_mean': [27.99144619, 19.51839209, 16.42992409],
     'img_std': [64.7680645, 47.09696396, 41.13751611],
     'img_target_size': (96, 128),
-    'model_path': '/CNN/models/CNN_binary_v1.h5',
+    'model_path': os.path.join(BASE_DIR,'CNN/models/CNN_binary_v1.h5'),
     'data_path': 'data/ISIC'
 }
