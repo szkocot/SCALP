683af7492892cde5269246e7b20643ff8dfc1f15
xsinitrax <kamila750@wp.pl>
2019-02-07 19:00:39 +0100

    remove redundant

--- db/0.11.sql
+++ db/0.11.sql
@@ -1,3 +1,2 @@
  -- admin user
 INSERT INTO public.users(username, name, surname, email, password, admin) VALUES ('admin', 'admin', 'admin', 'admin', '$2b$12$JJnDP/YkLln366KbQfQsb.IrI2Gih5jYI/BTx.XZocyWojXt1luby', true);
-ALTER TABLE public.users ADD UNIQUE (username);
