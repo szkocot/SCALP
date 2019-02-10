511c4a982814a1a34ebc1dea1c2867336f024df8
Szymon Kocot <szymko1995@gmail.com>
2019-02-10 15:00:47 +0100

    Rm dict element

--- config.py
+++ config.py
@@ -42,8 +42,7 @@ PREDICTOR = {
     'img_mean': [27.99144619, 19.51839209, 16.42992409],
     'img_std': [64.7680645, 47.09696396, 41.13751611],
     'img_target_size': (96, 128),
-    'model_path': os.path.join(BASE_DIR,'CNN/models/CNN_binary_v1.h5'),
-    'data_path': 'data/ISIC'
+    'model_path': os.path.join(BASE_DIR,'CNN/models/CNN_binary_v1.h5')
 }
 
 IMPORT = "dump" #json does import json / dump does dump db
